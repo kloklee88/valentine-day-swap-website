a4d244ebfa1e68716ab71b9fae6b5cc7e7c71124
Kevin <kloklee888@gmail.com>
2021-02-11 17:23:31 -0500

    Changed project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('virtual-white-elephant app is running!');
+    expect(page.getTitleText()).toEqual('valentine-day-swap-website app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'virtual-white-elephant'`, () => {
+  it(`should have as title 'valentine-day-swap-website'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('virtual-white-elephant');
+    expect(app.title).toEqual('valentine-day-swap-website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('virtual-white-elephant app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('valentine-day-swap-website app is running!');
   });
 });
